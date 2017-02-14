1f587550a56bc8deb64651200594075bd5bcf2c6
Elena Shishmakova <hella83@mail.ru>
2017-02-14 23:14:11 +0300

    создан класс Point

---
+++ sandbox/src/main/java/ru/stqa/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.sandbox;
+
+/**
+ * Created by Elena on 12.02.2017.
+ */
+public class Point {
+
+  public double a;
+  public double b;
+
+  public Point (double a,double b) {
+    this.a = a;
+    this.b = b;
+  }
+}
