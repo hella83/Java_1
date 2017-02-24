eaa0fad0abeaac265006a2f52b6d8d875224bb44
Elena Shishmakova <hella83@mail.ru>
2017-02-24 21:13:07 +0300

    Реализована возможность запуска тестов в IE, Firefox и Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import sun.plugin2.util.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,15 +13,26 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == org.openqa.selenium.remote.BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    } else if (browser == org.openqa.selenium.remote.BrowserType.CHROME){
+      wd = new ChromeDriver();
+    } else if (browser == org.openqa.selenium.remote.BrowserType.IE){
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(600, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     sessionHelper = new SessionHelper(wd);
