a68ae5b23305b05d8321d604e2d233221fcf1029
Elena Shishmakova <hella83@mail.ru>
2017-02-25 16:47:43 +0300

    == заменено на Objects.equals

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,8 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import sun.plugin2.util.BrowserType;
 
+
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,11 +27,11 @@ public class ApplicationManager {
   }
 
   public void init() {
-    if (browser == org.openqa.selenium.remote.BrowserType.FIREFOX){
+    if (Objects.equals(browser, org.openqa.selenium.remote.BrowserType.FIREFOX)){
       wd = new FirefoxDriver();
-    } else if (browser == org.openqa.selenium.remote.BrowserType.CHROME){
+    } else if (Objects.equals(browser, org.openqa.selenium.remote.BrowserType.CHROME)){
       wd = new ChromeDriver();
-    } else if (browser == org.openqa.selenium.remote.BrowserType.IE){
+    } else if (Objects.equals(browser, org.openqa.selenium.remote.BrowserType.IE)){
       wd = new InternetExplorerDriver();
     }
     wd.manage().timeouts().implicitlyWait(600, TimeUnit.SECONDS);
