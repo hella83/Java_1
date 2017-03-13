9e5bbb52b121ca8d9cf0b0c3e9dce6af12682fd3
Elena Shishmakova <hella83@mail.ru>
2017-03-13 23:34:20 +0300

    добавлена проверка количества контактов до/после создания, редактирования, удаления

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -13,10 +14,13 @@ public class ContactDeletionTests extends TestBase{
             app.getContactHelper().createContact(new ContactData("FName1", "LName1", null, null, null, "test1"));
         }
         app.getNavigationHelper().goToHomePage();
+        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectedContact();
         app.closeAlertWindow();
         app.getNavigationHelper().goToHomePage();
+        int after = app.getContactHelper().getContactCount();
+        Assert.assertEquals(after, before - 1);
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.ContactHelper;
 import ru.stqa.pft.addressbook.model.ContactData;
@@ -16,11 +17,13 @@ public class ContactModificationTests extends TestBase {
       app.getContactHelper().createContact(new ContactData("FName1", "LName1", null, null, null, "test1"));
     }
     app.getNavigationHelper().goToHomePage();
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("FName1", "LName1", "newTestAddress", "newTestPhone", "newTestMail", null),false);
     app.getContactHelper().submitContactmodification();
     app.getNavigationHelper().goToHomePage();
-
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before);
   }
 }
