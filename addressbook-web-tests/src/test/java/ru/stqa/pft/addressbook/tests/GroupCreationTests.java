a4bd96f643666665ae92d1256f4231e7f0f6b721
Elena Shishmakova <hella83@mail.ru>
2017-03-18 18:11:37 +0300

    добавление fluent-интерфейсов для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.goTo().groupPage();
     List<GroupData> before = app.group().list();
-    GroupData group = new GroupData("test1134561", null, null);
+    GroupData group = new GroupData().withName("test1134561");
     app.group().create(group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() + 1);
