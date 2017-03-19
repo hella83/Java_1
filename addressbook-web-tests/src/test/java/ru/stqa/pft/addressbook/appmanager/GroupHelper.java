040ad8e6a12701fde2e4e447eb98d193fc781580
Elena Shishmakova <hella83@mail.ru>
2017-03-19 15:21:36 +0300

    добавлено кэширование для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -57,6 +57,7 @@ public class GroupHelper extends BaseHelper {
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -65,12 +66,14 @@ public class GroupHelper extends BaseHelper {
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
+    groupCache = null;
     returnToGroupPage();
   }
 
   public void delete(GroupData group) {
     selectGroupById(group.getId());
     deleteSelectedGroup();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -87,15 +90,21 @@ public class GroupHelper extends BaseHelper {
    return wd.findElements(By.name("selected[]")).size();
   }
 
+  private Groups groupCache = null;
+
+
   public Groups all() {
-    Groups groups = new Groups();
+    if (groupCache != null) {
+      return  new Groups(groupCache);
+    }
+    groupCache = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements){
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groups.add(new GroupData().withId(id).withName(name));
+      groupCache.add(new GroupData().withId(id).withName(name));
     }
-    return groups;
+    return new Groups(groupCache);
 
   }
 
