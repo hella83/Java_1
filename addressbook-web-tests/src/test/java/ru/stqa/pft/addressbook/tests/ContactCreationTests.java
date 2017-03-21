9a18c26fcaf18631963183f26eba68195b7c0761
Elena Shishmakova <hella83@mail.ru>
2017-03-22 01:54:41 +0300

    сделаны провайдеры данных для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,24 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withFirstName(split[0]).withLastName(split[1])
+                    .withAddress(split[2]).withHomephone(split[3]).withEmail(split[4]).withPhoto(new File(split[5]))});
+            line = reader.readLine();
+        }
+        return  list.iterator();
+    }
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) {
         app.goTo().home();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/test.png");
-        ContactData contact = new ContactData().withFirstName("FNamePhoto1").withLastName("LName1111")
-                .withAddress("Moscow").withEmail("111@111")
-                .withHomephone("7").withMobilephone("65").withWorkphone("3").withPhoto(photo);
         app.contact().create(contact);
         app.goTo().home();
         Contacts after = app.contact().all();
