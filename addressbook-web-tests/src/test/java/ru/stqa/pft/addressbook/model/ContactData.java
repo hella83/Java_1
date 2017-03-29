9b37c6d4e76e82778fd7eb2a276f1016cd63dbfc
Elena Shishmakova <hella83@mail.ru>
2017-03-29 16:27:09 +0300

    извлечение из бд контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -2,27 +2,63 @@ package ru.stqa.pft.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
-
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
   @XStreamOmitField
-  private int id = Integer.MAX_VALUE;;
+  @Id
+  @Column(name = "id")
+  private int id = Integer.MAX_VALUE;
+
+  @Column(name = "firstname")
   private  String firstname;
+
+  @Column(name = "lastname")
   private  String lastname;
+
+  @Column(name = "address")
+  @Type(type = "text")
   private  String address;
+
+  @Column(name = "home")
+  @Type(type = "text")
   private  String homephone;
+
+  @Column(name = "mobile")
+  @Type(type = "text")
   private  String mobilephone;
+
+  @Column(name = "work")
+  @Type(type = "text")
   private  String workphone;
+
+  @Column(name = "email")
+  @Type(type = "text")
   private  String email;
+
+  @Column(name = "email2")
+  @Type(type = "text")
   private  String email2;
+
+  @Column(name = "email3")
+  @Type(type = "text")
   private  String email3;
+
+  @Transient
   private String group;
+  @Transient
   private  String allphones;
+  @Transient
   private  String allemails;
-  private File photo;
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public ContactData withId(int id) {
     this.id = id;
@@ -30,11 +66,11 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
